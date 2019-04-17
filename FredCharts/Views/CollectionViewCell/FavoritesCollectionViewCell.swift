acbf07072a100a967151b606aec6b90f03c3f7c8
Ben Hakes <bhhakes@gmail.com>
2019-04-17 12:47:39 -0500

    add README.md and photos

--- FredCharts/Views/CollectionViewCell/FavoritesCollectionViewCell.swift
+++ FredCharts/Views/CollectionViewCell/FavoritesCollectionViewCell.swift
@@ -20,18 +20,19 @@ class FavoritesCollectionViewCell: UICollectionViewCell {
     func updateViews(){
         titleLabel.adjustsFontSizeToFitWidth = true
         titleLabel.minimumScaleFactor = 0.5
-        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.cornerRadius = 10
         self.contentView.layer.borderWidth = 1.0
+        
         self.contentView.layer.borderColor = UIColor.clear.cgColor
         self.contentView.layer.masksToBounds = true
         
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 3.0)
-        self.layer.shadowRadius = 3.0
-        self.layer.shadowOpacity = 0.5
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 1.0
+        self.layer.cornerRadius = 8
         self.layer.masksToBounds = false
-        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
-        self.layer.cornerRadius = 12
+        self.layer.shadowPath = UIBezierPath(roundedRect:self.bounds, cornerRadius:self.contentView.layer.cornerRadius).cgPath
     }
     
     // MARK: - Properties
---
+++ README.md
@@ -0,0 +1,21 @@
+Fred Charts
+====
+
+FredCharts is an iOS app that can search, graph, and save data from St. Louis Federal Reserve Bank's [FRED API web
+service](https://api.stlouisfed.org/docs/fred/). FredCharts is largely similar to the St. Louis's FED [existing iOS app](https://itunes.apple.com/us/app/fred-economic-data/id451715489?mt=8). 
+
+For more information on the FRED API Documentation:
+[http://api.stlouisfed.org/docs/fred/](https://api.stlouisfed.org/docs/fred/)
+
+![](Photos/pic1.png)
+![](Photos/pic2.png)
+![](Photos/pic3.png)
+![](Photos/pic4.png)
+
+## Features
+
+- [1] Search through the FRED API data set
+- [2] View data in beautifully drawn charts
+- [3] Adjust charts by specific date and time period and press to get details about data points
+- [4] Save your favorite charts for later review
+- [5] Charts automatically update the next time you run the app (assuming there's more data for the particular chart you're interested in)
